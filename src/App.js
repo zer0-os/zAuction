aa4fccc2d2fa6f038b8c184e75a647b907888d65
Andy Church <ilioscio@gmail.com>
2021-03-23 13:43:44 -0600

    +web3-react,+Connect button,+injected web3 providers now connect via the connect button,more providers commented out for now

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
-import { useWeb3React, Web3ReactProvider } from '@web3-react/core';
-import { Web3Provider } from '@ethersproject/providers';
+import { Web3ReactProvider, useWeb3React, UnsupportedChainIdError } from '@web3-react/core'
+import {
+  NoEthereumProviderError,
+  UserRejectedRequestError as UserRejectedRequestErrorInjected
+} from '@web3-react/injected-connector'
+import { UserRejectedRequestError as UserRejectedRequestErrorWalletConnect } from '@web3-react/walletconnect-connector'
+import { UserRejectedRequestError as UserRejectedRequestErrorFrame } from '@web3-react/frame-connector'
+import { Web3Provider } from '@ethersproject/providers'
+import { formatEther } from '@ethersproject/units'
+
+import { useEagerConnect, useInactiveListener } from './hooks'
+import {
+  injected,
+  network,
+  walletconnect,
+  walletlink,
+  ledger,
+  trezor,
+  lattice,
+  frame,
+  authereum,
+  fortmatic,
+  magic,
+  portis,
+  torus
+} from './connectors'
 
 import './App.css';
 import nft from './nft.png';
@@ -9,6 +33,23 @@ function getLibrary(provider) {
   const library = new Web3Provider(provider);
   library.pollingInterval = 12000;
   return library;
+}
+
+function getErrorMessage(error) {
+  if (error instanceof NoEthereumProviderError) {
+    return 'No Ethereum browser extension detected, install MetaMask on desktop or visit from a dApp browser on mobile.'
+  } else if (error instanceof UnsupportedChainIdError) {
+    return "You're connected to an unsupported network."
+  } else if (
+    error instanceof UserRejectedRequestErrorInjected ||
+    error instanceof UserRejectedRequestErrorWalletConnect ||
+    error instanceof UserRejectedRequestErrorFrame
+  ) {
+    return 'Please authorize this website to access your Ethereum account.'
+  } else {
+    console.error(error)
+    return 'An unknown error occurred. Check the console for more details.'
+  }
 }
 
 function App() {
