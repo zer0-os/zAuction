d0040f7493bc0cda4cb62ad266bdc1bd1ab92448
Andy Church <ilioscio@gmail.com>
2021-04-24 23:06:17 -0600

    +create view changes, +fleek proxy changes

--- proxy/src/api/fleek.js
+++ proxy/src/api/fleek.js
@@ -11,108 +11,123 @@ const limiter = rateLimit({
 const router = express.Router();
 
 const secrets = {
-  apiKey: process.env.REACT_APP_FLEEK_API_KEY,
-  apiSecret: process.env.REACT_APP_FLEEK_API_SECRET,
-	key: 'test/0/2.json',
-	data: {
-		name: 'value',
-		age: 4,
-		value: false,
-	}
+  apiKey: "",
+  apiSecret: "",
+  cashew: "50",
+  key: 'test/0/2.json',
+  data: {
+    name: 'value',
+    age: 4,
+    value: false,
+  }
 }
 
+function checkNullCreateFields (...args) {
+  let createFields = [];
+  for(let i = 0;i < args.length; i++){
+    if (args[i] == null || (!/\S/.test(args[i]))) {
+      return {data: createFields[i], value:false};
+    }
+  }
+  return{data:null, value:true};
+}
 async function fleekGetFile(hash) {
 	return myFile = await fleek.getFileFromHash({
 		hash: hash,
 	})
 }
 async function fleekListFiles(apiKey, apiSecret) {
-	return files = await fleekStorage.listFiles({
-	  apiKey: apiKey,
-	  apiSecret: apiSecret,
-	  getOptions: [
-	    'bucket',
-	    'key',
-	    'hash',
-	    'publicUrl'
-	  ],
-	})
+  return files = await fleekStorage.listFiles({
+    apiKey: apiKey,
+    apiSecret: apiSecret,
+    getOptions: [
+      'bucket',
+      'key',
+      'hash',
+      'publicUrl'
+    ],
+  })
 }
 async function fleekDeleteFile(apiKey, apiSecret, key, bucket) {
-	const input = {
-		apiKey: apiKey,
-		apiSecret: apiSecret,
-		key: key,
-		bucket: bucket
-	};
-	return result = await fleek.deleteFile(input);
+  const input = {
+    apiKey: apiKey,
+    apiSecret: apiSecret,
+    key: key,
+    bucket: bucket
+  };
+  return result = await fleek.deleteFile(input);
 }
 async function fleekWriteFile(apiKey, apiSecret, key, data) {
-	const input = {
-		apiKey: apiKey,
-		apiSecret: apiSecret,
-		key: key,
-		data: JSON.stringify(data)
-	};
-	return result = await fleek.upload(input);
+  const input = {
+    apiKey: apiKey,
+    apiSecret: apiSecret,
+    key: key,
+    data: JSON.stringify(data)
+  };
+  return result = await fleek.upload(input);
 }
 async function fleekGetBuckets(apiKey, apiSecret) {
-	const buckets = await fleek.listBuckets({
-		apiKey: apiKey,
-		apiSecret: apiSecret,
-	})
+  const buckets = await fleek.listBuckets({
+    apiKey: apiKey,
+    apiSecret: apiSecret,
+  })
 }
 
 let cachedData;
 let cacheTime;
 
-// Get bids endpoint
-router.get('/getBids', limiter, async (req, res, next) => {
-  console.log(process.env.RATE_LIMIT);
-  if (cacheTime && cacheTime > Date.now() - parseInt(process.env.CACHE_TIME) * 1000) {
-		return res.json(cachedData);
+// Get auction endpoint
+router.get('/getAuction', limiter, async (req, res, next) => {
+  if (cacheTime && cacheTime > Date.now() - parseInt(secrets.cashew) * 1000) {
+    return res.json(cachedData);
   }
   try {
-		await fleek.getFileFromHash({
-			hash: 'bafybeibtedlr5m36oxnzh5ylyauy6vpmchx4fbmdlbkoda3ztptkwbydty',
-		}).then((file) => {
-		  // Careful, this cache is stored in memory
-			cachedData = file;
-			cacheTime = Date.now();
-			res.json(file)
-		}
-		);
+    await fleek.getFileFromHash({
+      hash: 'bafybeibtedlr5m36oxnzh5ylyauy6vpmchx4fbmdlbkoda3ztptkwbydty',
+    }).then((file) => {
+      // Careful, this cache is stored in memory
+      cachedData = file;
+      cacheTime = Date.now();
+      res.json(file)
+    }
+    );
   } catch (error) {
     next(error);
   }
 });
 
-// Create bid endpoint
-router.get('/createBid', async (req, res, next) => {
+// Create auction endpoint
+router.get('/createAuction', async (req, res, next) => {
   try {
-		await fleek.upload({
-			apiKey: process.env.FLEEK_API_KEY,
-			apiSecret: process.env.FLEEK_API_SECRET,
-			key: secrets.key,
-			data: JSON.stringify(secrets.data)
-		}).then((file) => res.json(file));
+    var result = checkNullCreateFields();
+    if (result['value'] == false) {
+        return res.send({"status": "false", "message": result['data'] + " not found"});
+    }
+    await fleek.upload({
+      apiKey: secrets.apiKey,
+      apiSecret: secrets.apiSecret,
+      key: secrets.key,
+      data: JSON.stringify(secrets.data)
+    }).then((file) => res.json(file));
   } catch (error) {
     next(error);
   }
 });
 
-// List bids endpoint
-router.get('/listBids', async (req, res, next) => {
+// List auctions endpoint
+router.get('/listAuctions', async (req, res, next) => {
+  var t = Math.floor(new Date().getTime() / 1000);
+  console.log(t);
   try {
-		await fleek.listFiles({
-		  apiKey: process.env.FLEEK_API_KEY,
-		  apiSecret: process.env.FLEEK_API_SECRET,
-		  getOptions: [
-		    'bucket',
-		    'key',
-		    'publicUrl'
-		  ],
-		}).then((file) => res.json(file));
+    await fleek.listFiles({
+      apiKey: secrets.apiKey,
+      apiSecret: secrets.apiSecret,
+      getOptions: [
+        'bucket',
+        'key',
+        'publicUrl'
+      ],
+    }).then((file) => res.json(file));
   } catch (error) {
     next(error);
   }
--- src/components/CreateView/CreateView.tsx
+++ src/components/CreateView/CreateView.tsx
@@ -1,12 +1,17 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { useWeb3React } from '@web3-react/core';
 import * as ethers from 'ethers';
 import fleek from '@fleekhq/fleek-storage-js';
+
+import './CreateView.css';
 
 import nft from '../../nft.png';
 
 const CreateView = (props) => {
   const { account, library } = useWeb3React();
+  // contract address and token id of auction nft
+  const [ CA, setCA ] = useState(null);
+  const [ TI, setTI ] = useState(null);
 
   const nftcontractaddress = "0x101eD6EeC2CB7813a04614cA97421119219AfC1a";
   const nfttokenid = "0x0";
@@ -197,42 +202,44 @@ const CreateView = (props) => {
   		data: JSON.stringify(data)
   	};
   }
+
+  function createClick(e) {
+    e.preventDefault();
+    var t = Math.floor(new Date().getTime() / 1000);
+    console.log(t + '-' + account);
+  }
   
   return (
     <div className="create-view">
       <h1 className="title">Auction your NFT</h1>
       
-      <div className="nft_container" style={{display:"block"}}>
-        <img src={nft} className="nft_img" alt="nft" />
-        <div className="nft_bid_accept">
-          <form className="bid_form">
-            <input 
-              className="bid_input" 
-              type="number" 
-              value={bidAmt}
-              onChange={(e) => {
-                bidAmt = parseInt(e.target.value)
-              }}
-            />       
-            <button
-              className="bid_btn nft-btn"
-              onClick={(e) => bid(bidAmt,e)}
-            >
-              Bid
-            </button>
-          </form>
-          <div>
-            <button
-              className="accept_btn nft-btn"
-              onClick={accept}
-            >
-              Accept
-            </button>
-          </div>
-
-        </div>
-      </div>
+      <form className="create-form">
+        <h3 className="tshadow">Contract Address</h3>
+        <input 
+          className="zinput"
+          type="string"
+          onChange={e => {
+            setCA(e.target.value)
+          }}
+          
+        />
+        <h3 className="tshadow">Token Id</h3>
+        <input 
+          className="zinput"
+          type="string"
+          onChange={e => {
+            setTI(e.target.value);
+          }}
+        />
+        <button
+          className="create-btn"
+          onClick={createClick}
+        >
+          Accept
+        </button>
+      </form>
+      
     </div>
-  );
+  )
 }
 export default CreateView;
