5bfe7b41244273772a232d9c0229681980da6846
Leonel Salvo <agustin.leonel.salvo@hotmail.com>
2021-09-08 20:54:51 -0300

    Changed to use hardhat, created from 0 using zns branch contracts

--- src/redux/actions/ex-actions.ts
+++
@@ -1,6 +0,0 @@
-import * as types from '../constants/ActionTypes';
-
-export const setExValue = (exValue: any) => ({
-  type: 'SET_EXVALUE',
-  payload: exValue
-});
--- src/redux/reducers/root-reducer.ts
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-
-import exReducer from './ex-reducer';
-
-export default combineReducers({
-  exReducer: exReducer
-})
