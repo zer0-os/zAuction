345c436062c6b55939fb5bda74d3b935620c05a4
James Earle <JamesEarle@users.noreply.github.com>
2021-10-06 11:57:10 -0700

    Enable variable royalties to minter and owner of NFTs, include ability to set royalty for a given ID (#10)
    * passing tests for royalty calculations
    
    * working tests, updated zAuction contract
    
    * pr feedback and restricting access control
    
    * WIP weird hardhat testing issues with chai
    
    * all tests passing, small issue resolved with
    
    * final small change to modify logic in royalty calculation
    
    * add comments to public functions, update test case
    
    * remove sinon

--- contracts/Zauction.sol
+++ contracts/Zauction.sol
@@ -16,7 +16,10 @@ contract ZAuction is Initializable, OwnableUpgradeable {
 
   // Original zAuction contract address for backward compatibility
   address legacyZAuction;
+
   mapping(address => mapping(uint256 => bool)) public consumed;
+  mapping(uint256 => uint256) public topLevelDomainIdCache;
+  mapping(uint256 => uint256) public topLevelDomainFee;
 
   event BidAccepted(
     uint256 auctionId,
@@ -44,7 +47,6 @@ contract ZAuction is Initializable, OwnableUpgradeable {
   /// @param auctionId unique per address auction identifier chosen by seller
   /// @param bidder address of who the seller says the bidder is, for confirmation of the recovered bidder
   /// @param bid token amount bid
-  /// @param nftAddress contract address of the nft we are transferring
   /// @param tokenId token id we are transferring
   /// @param minbid minimum bid allowed
   /// @param startBlock block number at which acceptBid starts working
@@ -54,7 +56,6 @@ contract ZAuction is Initializable, OwnableUpgradeable {
     uint256 auctionId,
     address bidder,
     uint256 bid,
-    address nftAddress,
     uint256 tokenId,
     uint256 minbid,
     uint256 startBlock,
@@ -68,7 +69,7 @@ contract ZAuction is Initializable, OwnableUpgradeable {
     bytes32 data = createBid(
       auctionId,
       bid,
-      nftAddress,
+      address(registrar),
       tokenId,
       minbid,
       startBlock,
@@ -80,7 +81,7 @@ contract ZAuction is Initializable, OwnableUpgradeable {
       bytes32 legacyData = createLegacyBid(
         auctionId,
         bid,
-        nftAddress,
+        address(registrar),
         tokenId,
         minbid,
         startBlock,
@@ -93,33 +94,92 @@ contract ZAuction is Initializable, OwnableUpgradeable {
     }
     require(!consumed[bidder][auctionId], "zAuction: data already consumed");
 
-    // Will truncate any decimals
-    uint256 royalty = bid / 10;
+    uint256 topLevelId = topLevelDomainIdCache[tokenId];
+    if (topLevelId == 0) {
+      topLevelId = topLevelDomainIdOf(tokenId);
+      topLevelDomainIdCache[tokenId] = topLevelId;
+    }
 
-    IERC721 nftContract = IERC721(nftAddress);
     consumed[bidder][auctionId] = true;
-    // Bidder -> Owner, send funds
-    SafeERC20.safeTransferFrom(token, bidder, msg.sender, bid - royalty);
-    // Bidder -> Registrar, pay royalty
-    SafeERC20.safeTransferFrom(
-      token,
-      bidder,
-      registrar.minterOf(tokenId),
-      royalty
-    );
+
+    // Transfer payment, royalty to minter, and fee to topLevel domain
+    paymentTransfers(bidder, bid, msg.sender, topLevelId, tokenId);
+
     // Owner -> Bidder, send NFT
-    nftContract.safeTransferFrom(msg.sender, bidder, tokenId);
+    registrar.safeTransferFrom(msg.sender, bidder, tokenId);
+
     emit BidAccepted(
       auctionId,
       bidder,
       msg.sender,
       bid,
-      address(nftContract),
+      address(registrar),
       tokenId,
       expireBlock
     );
   }
 
+  /// Allows the owner of the given token to set the fee owed upon sale
+  /// Amount given should be as a percent with 5 decimals of precision
+  /// e.g. 10% (max) is 1000000, 0.0001% (min) is 1
+  /// @param id The id of the domain to update
+  /// @param amount The
+  function setTopLevelDomainFee(uint256 id, uint256 amount) public {
+    require(
+      msg.sender == registrar.ownerOf(id),
+      "zAuction: Cannot set fee on unowned domain"
+    );
+    require(amount <= 1000000, "zAuction: Cannot set a fee higher than 10%");
+    require(amount != topLevelDomainFee[id], "zAuction: Amount is already set");
+    topLevelDomainFee[id] = amount;
+  }
+
+  /// Fetch the top level domain fee if it exists and calculate the token amount
+  /// @param topLevelId The id of the top level domain for a subdomain
+  /// @param bid The bid for the fee to apply to
+  function calculateTopLevelDomainFee(uint256 topLevelId, uint256 bid)
+    public
+    view
+    returns (uint256)
+  {
+    require(topLevelId > 0, "zAuction: must provide a valid id");
+    require(bid > 0, "zAuction: Cannot calculate domain fee on an empty bid");
+
+    // Find what percent they've specified as a royalty
+    uint256 fee = topLevelDomainFee[topLevelId];
+    if (fee == 0) return 0;
+
+    uint256 calculatedFee = (bid * fee * 10**13) / (100 * 10**18);
+
+    return calculatedFee;
+  }
+
+  /// Fetch the minter royalty if it exists and calculate the token amount
+  /// @param bid The bid for the royalty to be calculated
+  /// @param id The id of the minted domain
+  function calculateMinterRoyalty(uint256 id, uint256 bid)
+    public
+    view
+    returns (uint256)
+  {
+    require(id > 0, "zAuction: must provide a valid id");
+    // Returns with 5 decimal points of accuracy
+    uint256 domainRoyalty = registrar.domainRoyaltyAmount(id);
+    if (domainRoyalty == 0) return 0; // same here, 0 for 0% or 0 for not found?
+
+    uint256 royalty = (bid * domainRoyalty * 10**13) / (100 * 10**18);
+
+    return royalty;
+  }
+
+  /// Create a bid object hashed with the current contract address
+  /// @param auctionId unique per address auction identifier chosen by seller
+  /// @param bid token amount bid
+  /// @param nftAddress address of the nft contract
+  /// @param tokenId token id we are transferring
+  /// @param minbid minimum bid allowed
+  /// @param startBlock block number at which acceptBid starts working
+  /// @param expireBlock block number at which acceptBid stops working
   function createBid(
     uint256 auctionId,
     uint256 bid,
@@ -145,6 +205,15 @@ contract ZAuction is Initializable, OwnableUpgradeable {
     return data;
   }
 
+  /// Create a bid object hashed with the legacy contract address
+  /// for backwards compatability.
+  /// @param auctionId unique per address auction identifier chosen by seller
+  /// @param bid token amount bid
+  /// @param nftAddress address of the nft contract
+  /// @param tokenId token id we are transferring
+  /// @param minbid minimum bid allowed
+  /// @param startBlock block number at which acceptBid starts working
+  /// @param expireBlock block number at which acceptBid stops working
   function createLegacyBid(
     uint256 auctionId,
     uint256 bid,
@@ -170,6 +239,20 @@ contract ZAuction is Initializable, OwnableUpgradeable {
     return data;
   }
 
+  // Will return self if already at the top level
+  function topLevelDomainIdOf(uint256 id) public view returns (uint256) {
+    uint256 parentId = registrar.parentOf(id);
+    uint256 holder = id;
+    while (parentId != 0) {
+      holder = parentId; // Hold on to previous parent
+      parentId = registrar.parentOf(parentId);
+    }
+    return holder;
+  }
+
+  /// Recover an account from a signature hash
+  /// @param hash the bytes object
+  /// @param signature the signature to recover from
   function recover(bytes32 hash, bytes memory signature)
     public
     pure
@@ -180,5 +263,40 @@ contract ZAuction is Initializable, OwnableUpgradeable {
 
   function toEthSignedMessageHash(bytes32 hash) public pure returns (bytes32) {
     return hash.toEthSignedMessageHash();
+  }
+
+  /// Send all required payment transfers when an NFT is sold
+  /// This requires paying the owner, minter, and top level domain owner
+  /// @param bidder address of who the seller says the bidder is, for confirmation of the recovered bidder
+  /// @param bid token amount bid
+  /// @param owner address of the owner of that domain pre-transfer
+  /// @param topLevelId the ID of the top level domain for a given domain or subdomain
+  /// @param tokenId the ID of the domain
+  function paymentTransfers(
+    address bidder,
+    uint256 bid,
+    address owner,
+    uint256 topLevelId,
+    uint256 tokenId
+  ) internal {
+    address topLevelOwner = registrar.ownerOf(topLevelId);
+    uint256 topLevelFee = calculateTopLevelDomainFee(topLevelId, bid);
+    uint256 minterRoyalty = calculateMinterRoyalty(tokenId, bid);
+
+    uint256 bidActual = bid - minterRoyalty - topLevelFee;
+
+    // Bidder -> Owner, pay transaction
+    SafeERC20.safeTransferFrom(token, bidder, owner, bidActual);
+
+    // Bidder -> Minter, pay minter royalty
+    SafeERC20.safeTransferFrom(
+      token,
+      bidder,
+      registrar.minterOf(tokenId),
+      minterRoyalty
+    );
+
+    // Bidder -> topLevel Owner, pay top level owner fee
+    SafeERC20.safeTransferFrom(token, bidder, topLevelOwner, topLevelFee);
   }
 }
--- test/zauction.test.ts
+++ test/zauction.test.ts
@@ -5,8 +5,6 @@ import { FakeContract, smock } from "@defi-wonderland/smock";
 import {
   IERC20,
   IERC20__factory,
-  IERC721,
-  IERC721__factory,
   IRegistrar,
   IRegistrar__factory,
   ZAuction,
@@ -24,11 +22,7 @@ describe("zAuction Contract Tests", () => {
   let owner: SignerWithAddress;
   let zAuction: ZAuction;
   let mockERC20Token: FakeContract<IERC20>;
-  let mockERC721: FakeContract<IERC721>;
   let mockRegistrar: FakeContract<IRegistrar>;
-
-  const tokenId = "0x1";
-  const nftContract = "0xC613fCc3f81cC2888C5Cccc1620212420FFe4931";
 
   before(async () => {
     const signers: SignerWithAddress[] = await ethers.getSigners();
@@ -37,24 +31,127 @@ describe("zAuction Contract Tests", () => {
     owner = signers[2];
 
     mockERC20Token = await smock.fake(IERC20__factory.abi);
-    mockERC721 = await smock.fake(IERC721__factory.abi);
     mockRegistrar = await smock.fake(IRegistrar__factory.abi);
+
+    // Royalty is fixed at 10% unless otherwise specified
+    mockRegistrar.domainRoyaltyAmount.returns(1000000);
 
     const zAuctionFactory = new ZAuction__factory(creator);
     zAuction = await zAuctionFactory.deploy();
-    const zAuctionV1KovanAddress = "0x18A804a028aAf1F30082E91d2947734961Dd7f89";
+    const legacyZAuctionKovanAddress =
+      "0x18A804a028aAf1F30082E91d2947734961Dd7f89";
     await zAuction.initialize(
       mockERC20Token.address,
       mockRegistrar.address,
-      zAuctionV1KovanAddress
+      legacyZAuctionKovanAddress
     );
+  });
+
+  it("Successfully tests a specific scenario", async () => {
+    const signers: SignerWithAddress[] = await ethers.getSigners();
+
+    const minter = signers[0]; // User Z, gets royalty of 5%
+    const topLevelOwner = signers[1]; // User W, Owns A, 4.44% fee
+    const seller = signers[2]; // User X, selling ABC
+    const buyer = signers[3]; // user Y, buying ABC, bids 123
+    // const id = "2";
+    const topLevelId = "1";
+
+    const bidParams = {
+      auctionId: "4771690347",
+      bid: ethers.utils.parseEther("123"),
+      tokenId: "0x1",
+      minBid: "0",
+      startBlock: "0",
+      expireBlock: "999999999999",
+    };
+
+    // Minter royalty is set at 5%
+    mockRegistrar.domainRoyaltyAmount
+      .whenCalledWith(bidParams.tokenId)
+      .returns("500000");
+
+    // Top level owner fee is set at 4.44%
+    mockRegistrar.parentOf
+      .whenCalledWith(bidParams.tokenId)
+      .returns(topLevelId);
+    mockRegistrar.parentOf.whenCalledWith(topLevelId).returns("0");
+    mockRegistrar.ownerOf
+      .whenCalledWith(topLevelId)
+      .returns(topLevelOwner.address);
+    mockRegistrar.minterOf.whenCalledWith(topLevelId).returns(minter.address);
+
+    await zAuction
+      .connect(topLevelOwner)
+      .setTopLevelDomainFee(topLevelId, 444000);
+
+    const bidToSign = await zAuction.createBid(
+      bidParams.auctionId,
+      bidParams.bid,
+      mockRegistrar.address,
+      bidParams.tokenId,
+      bidParams.minBid,
+      bidParams.startBlock,
+      bidParams.expireBlock
+    );
+
+    const signature = await buyer.signMessage(ethers.utils.arrayify(bidToSign));
+
+    mockERC20Token.transferFrom.returns(true);
+    await zAuction
+      .connect(seller)
+      .acceptBid(
+        signature,
+        bidParams.auctionId,
+        buyer.address,
+        bidParams.bid,
+        bidParams.tokenId,
+        bidParams.minBid,
+        bidParams.startBlock,
+        bidParams.expireBlock
+      );
+
+    const transactionPayment = ethers.utils.parseEther("111.3888");
+    const royalty = ethers.utils.parseEther("6.150");
+    const fee = ethers.utils.parseEther("5.4612");
+
+    expect(mockRegistrar.domainRoyaltyAmount).to.have.been.calledWith(
+      topLevelId
+    );
+    expect(mockRegistrar.parentOf).to.have.been.calledWith(bidParams.tokenId);
+    expect(mockRegistrar.parentOf).to.have.been.calledWith(topLevelId);
+    expect(mockRegistrar.ownerOf).to.have.been.calledWith(topLevelId);
+
+    // Bidder -> Owner, pay transaction
+    expect(mockERC20Token.transferFrom).calledWith(
+      buyer.address,
+      seller.address,
+      transactionPayment
+    );
+
+    // Bidder -> Minter, pay minter royalty
+    expect(mockERC20Token.transferFrom).to.have.been.calledWith(
+      buyer.address,
+      minter.address,
+      royalty
+    );
+
+    // Bidder -> topLevel Owner, pay top level owner fee
+    expect(mockERC20Token.transferFrom).to.have.been.calledWith(
+      buyer.address,
+      topLevelOwner.address,
+      fee
+    );
+
+    mockRegistrar.domainRoyaltyAmount.reset();
+    mockRegistrar.parentOf.reset();
+    mockRegistrar.ownerOf.reset();
   });
 
   it("Successfully accepts a bid", async () => {
     const bidParams = {
       auctionId: "4771690347",
       bid: "2000000000000000000",
-      nftAddress: mockERC721.address,
       tokenId: "0x1",
       minBid: "0",
       startBlock: "0",
@@ -64,15 +161,12 @@ describe("zAuction Contract Tests", () => {
     const bidToSign = await zAuction.createBid(
       bidParams.auctionId,
       bidParams.bid,
-      bidParams.nftAddress,
+      mockRegistrar.address,
       bidParams.tokenId,
       bidParams.minBid,
       bidParams.startBlock,
       bidParams.expireBlock
     );
-
-    const signers = await ethers.getSigners();
-    const bidder = signers[1];
 
     const signature = await bidder.signMessage(
       ethers.utils.arrayify(bidToSign)
@@ -81,7 +175,6 @@ describe("zAuction Contract Tests", () => {
     // In the case of a resale, the original creator
     // of an NFT is not necessarily the same as the
     // person who is the owner
-    const owner = signers[2];
     mockERC20Token.transferFrom.returns(true);
 
     // Note added "signature" and "bidder.address" props
@@ -92,7 +185,6 @@ describe("zAuction Contract Tests", () => {
         bidParams.auctionId,
         bidder.address,
         bidParams.bid,
-        bidParams.nftAddress,
         bidParams.tokenId,
         bidParams.minBid,
         bidParams.startBlock,
@@ -103,8 +195,7 @@ describe("zAuction Contract Tests", () => {
     const bidParams = {
       auctionId: "4771690347",
       bid: "2000000000000000000",
-      nftAddress: nftContract,
-      tokenId: tokenId,
+      tokenId: "0x1",
       minBid: "0",
       startBlock: BigNumber.from("999999999999"),
       expireBlock: "1",
@@ -113,7 +204,7 @@ describe("zAuction Contract Tests", () => {
     const bidToSign = await zAuction.createBid(
       bidParams.auctionId,
       bidParams.bid,
-      bidParams.nftAddress,
+      mockRegistrar.address,
       bidParams.tokenId,
       bidParams.minBid,
       bidParams.startBlock,
@@ -131,7 +222,6 @@ describe("zAuction Contract Tests", () => {
         bidParams.auctionId,
         bidder.address,
         bidParams.bid,
-        bidParams.nftAddress,
         bidParams.tokenId,
         bidParams.minBid,
         bidParams.startBlock,
@@ -143,8 +233,7 @@ describe("zAuction Contract Tests", () => {
     const bidParams = {
       auctionId: "4771690347",
       bid: "2000000000000000000",
-      nftAddress: nftContract,
-      tokenId: tokenId,
+      tokenId: "0x1",
       minBid: "0",
       startBlock: "0",
       expireBlock: "1",
@@ -153,7 +242,7 @@ describe("zAuction Contract Tests", () => {
     const bidToSign = await zAuction.createBid(
       bidParams.auctionId,
       bidParams.bid,
-      bidParams.nftAddress,
+      mockRegistrar.address,
       bidParams.tokenId,
       bidParams.minBid,
       bidParams.startBlock,
@@ -171,7 +260,6 @@ describe("zAuction Contract Tests", () => {
         bidParams.auctionId,
         bidder.address,
         bidParams.bid,
-        bidParams.nftAddress,
         bidParams.tokenId,
         bidParams.minBid,
         bidParams.startBlock,
@@ -183,8 +271,7 @@ describe("zAuction Contract Tests", () => {
     const bidParams = {
       auctionId: "4771690347",
       bid: "2000000000000000000",
-      nftAddress: nftContract,
-      tokenId: tokenId,
+      tokenId: "0x1",
       minBid: "5000000000000000000",
       startBlock: "0",
       expireBlock: "999999999999",
@@ -193,7 +280,7 @@ describe("zAuction Contract Tests", () => {
     const bidToSign = await zAuction.createBid(
       bidParams.auctionId,
       bidParams.bid,
-      bidParams.nftAddress,
+      mockRegistrar.address,
       bidParams.tokenId,
       bidParams.minBid,
       bidParams.startBlock,
@@ -211,7 +298,6 @@ describe("zAuction Contract Tests", () => {
         bidParams.auctionId,
         bidder.address,
         bidParams.bid,
-        bidParams.nftAddress,
         bidParams.tokenId,
         bidParams.minBid,
         bidParams.startBlock,
@@ -225,8 +311,7 @@ describe("zAuction Contract Tests", () => {
     const bidParams = {
       auctionId: "4771690347",
       bid: "2000000000000000000",
-      nftAddress: nftContract,
-      tokenId: tokenId,
+      tokenId: "0x1",
       minBid: "500000000",
       startBlock: "0",
       expireBlock: "999999999999",
@@ -235,7 +320,7 @@ describe("zAuction Contract Tests", () => {
     const bidToSign = await zAuction.createBid(
       bidParams.auctionId,
       bidParams.bid,
-      bidParams.nftAddress,
+      mockRegistrar.address,
       bidParams.tokenId,
       bidParams.minBid,
       bidParams.startBlock,
@@ -253,7 +338,6 @@ describe("zAuction Contract Tests", () => {
         bidParams.auctionId,
         owner.address,
         bidParams.bid,
-        bidParams.nftAddress,
         bidParams.tokenId,
         bidParams.minBid,
         bidParams.startBlock,
@@ -265,8 +349,7 @@ describe("zAuction Contract Tests", () => {
     const bidParams = {
       auctionId: "4771690347",
       bid: "2000000000000000000",
-      nftAddress: nftContract,
-      tokenId: tokenId,
+      tokenId: "0x1",
       minBid: "500000000",
       startBlock: "0",
       expireBlock: "999999999999",
@@ -275,7 +358,7 @@ describe("zAuction Contract Tests", () => {
     const bidToSign = await zAuction.createBid(
       bidParams.auctionId,
       bidParams.bid,
-      bidParams.nftAddress,
+      mockRegistrar.address,
       bidParams.tokenId,
       bidParams.minBid,
       bidParams.startBlock,
@@ -295,7 +378,6 @@ describe("zAuction Contract Tests", () => {
         bidParams.auctionId,
         creator.address,
         bidParams.bid,
-        bidParams.nftAddress,
         bidParams.tokenId,
         bidParams.minBid,
         bidParams.startBlock,
@@ -307,8 +389,7 @@ describe("zAuction Contract Tests", () => {
     const bidParams = {
       auctionId: "4771690347",
       bid: "2000000000000000000",
-      nftAddress: nftContract,
-      tokenId: tokenId,
+      tokenId: "0x1",
       minBid: "500000000",
       startBlock: "0",
       expireBlock: "999999999999",
@@ -317,7 +398,7 @@ describe("zAuction Contract Tests", () => {
     const bidToSign = await zAuction.createBid(
       bidParams.auctionId,
       bidParams.bid,
-      bidParams.nftAddress,
+      mockRegistrar.address,
       bidParams.tokenId,
       bidParams.minBid,
       bidParams.startBlock,
@@ -335,7 +416,6 @@ describe("zAuction Contract Tests", () => {
         bidParams.auctionId,
         bidder.address,
         bidParams.bid,
-        bidParams.nftAddress,
         bidParams.tokenId,
         bidParams.minBid,
         bidParams.startBlock,
@@ -343,5 +423,165 @@ describe("zAuction Contract Tests", () => {
       );
 
     await expect(tx).to.be.revertedWith("zAuction: data already consumed");
+  });
+  it("Calculates minter royalty correctly", async () => {
+    // A percent with 5 decimals of precision
+    mockRegistrar.domainRoyaltyAmount.returns(1000000);
+    // Each WILD is 10^18, Bid is 15 WILD
+    const bid = ethers.utils.parseEther("15");
+    const id = "12345";
+    const royalty = await zAuction.calculateMinterRoyalty(id, bid);
+    const decimal = royalty.toString();
+
+    // 10% of bid
+    expect(decimal).to.equal(ethers.utils.parseEther("1.5"));
+  });
+  it("Calculates top level domain fee correctly at 10%", async () => {
+    // Each WILD is 10^18, Bid is 15 WILD
+    // A percent with 5 decimals of precision
+    const bid = ethers.utils.parseEther("15");
+    const id = "123245";
+    const callers = await ethers.getSigners();
+    const mainAccount = callers[0];
+
+    // mockRegistrar.domainRoyaltyAmount.returns(1000000);
+    mockRegistrar.ownerOf.whenCalledWith(id).returns(mainAccount.address);
+
+    // Set fee for 10%
+    await zAuction.connect(mainAccount).setTopLevelDomainFee(id, 1000000);
+    let returnedFee = await zAuction.calculateTopLevelDomainFee(id, bid);
+    let fee = returnedFee.toString();
+    expect(fee).to.equal(ethers.utils.parseEther("1.5"));
+  });
+  it("Calculates top level domain fee correctly at 3%", async () => {
+    const bid = ethers.utils.parseEther("15");
+    const id = "123245";
+    const callers = await ethers.getSigners();
+    const mainAccount = callers[0];
+
+    mockRegistrar.ownerOf.whenCalledWith(id).returns(mainAccount.address);
+
+    // Set fee for 3%
+    await zAuction.connect(mainAccount).setTopLevelDomainFee(id, 300000);
+    const returnedFee = await zAuction.calculateTopLevelDomainFee(id, bid);
+    const fee = returnedFee.toString();
+    expect(fee).to.equal(ethers.utils.parseEther("0.45"));
+  });
+  it("Calculates top level domain fee correctly at 1.23456%", async () => {
+    const bid = ethers.utils.parseEther("15");
+    const id = "123245";
+    const callers = await ethers.getSigners();
+    const mainAccount = callers[0];
+
+    mockRegistrar.ownerOf.whenCalledWith(id).returns(mainAccount.address);
+
+    // Set fee for 1.23456%
+    await zAuction.connect(mainAccount).setTopLevelDomainFee(id, 123456);
+    const returnedFee = await zAuction.calculateTopLevelDomainFee(id, bid);
+    const fee = returnedFee.toString();
+    expect(fee).to.equal(ethers.utils.parseEther("0.185184"));
+  });
+  it("Calculates top level domain fee correctly at 9.99999%", async () => {
+    const bid = ethers.utils.parseEther("15");
+    const id = "123245";
+    const callers = await ethers.getSigners();
+    const mainAccount = callers[0];
+
+    mockRegistrar.ownerOf.whenCalledWith(id).returns(mainAccount.address);
+
+    // Set fee for 9.99999%
+    await zAuction.connect(mainAccount).setTopLevelDomainFee(id, 999999);
+    const returnedFee = await zAuction.calculateTopLevelDomainFee(id, bid);
+    const fee = returnedFee.toString();
+    expect(fee).to.equal(ethers.utils.parseEther("1.4999985"));
+  });
+  it("Calculates top level domain fee correctly at 9.11111%", async () => {
+    const bid = ethers.utils.parseEther("15");
+    const id = "123245";
+    const callers = await ethers.getSigners();
+    const mainAccount = callers[0];
+
+    mockRegistrar.ownerOf.whenCalledWith(id).returns(mainAccount.address);
+
+    // Set fee for 9.11111%
+    await zAuction.connect(mainAccount).setTopLevelDomainFee(id, 911111);
+    const returnedFee = await zAuction.calculateTopLevelDomainFee(id, bid);
+    const fee = returnedFee.toString();
+    expect(fee).to.equal(ethers.utils.parseEther("1.3666665"));
+  });
+  it("Calculates top level domain fee correctly at 0.0001%", async () => {
+    const bid = ethers.utils.parseEther("15");
+    const id = "123245";
+    const callers = await ethers.getSigners();
+    const mainAccount = callers[0];
+
+    mockRegistrar.ownerOf.whenCalledWith(id).returns(mainAccount.address);
+
+    // Set fee for 0.0001%%
+    await zAuction.connect(mainAccount).setTopLevelDomainFee(id, 1);
+    const returnedFee = await zAuction.calculateTopLevelDomainFee(id, bid);
+    const fee = returnedFee.toString();
+    expect(fee).to.equal(ethers.utils.parseEther("0.0000015"));
+  });
+  it("Fails to set top level royalty when domain is not owned by caller", async () => {
+    // Fee is set to 10%
+    const fee = "1000000";
+    const id = "123245";
+
+    const callers = await ethers.getSigners();
+    const mainAccount = callers[0];
+    const otherAccount = callers[1];
+
+    mockRegistrar.ownerOf.whenCalledWith(id).returns(mainAccount.address);
+
+    const tx = zAuction.connect(otherAccount).setTopLevelDomainFee(id, fee);
+    await expect(tx).to.be.revertedWith(
+      "zAuction: Cannot set fee on unowned domain"
+    );
+  });
+  it("Fails if the domain fee is set higher than 10%", async () => {
+    // Fee is set to 10%
+    const fee = "1000001";
+    const id = "123245";
+
+    const callers = await ethers.getSigners();
+    const mainAccount = callers[0];
+
+    mockRegistrar.ownerOf.whenCalledWith(id).returns(mainAccount.address);
+    const tx = zAuction.connect(mainAccount).setTopLevelDomainFee(id, fee);
+
+    await expect(tx).to.be.revertedWith(
+      "zAuction: Cannot set a fee higher than 10%"
+    );
+  });
+  it("Fails if the domain fee is already set to that amount", async () => {
+    // Fee is set to 5%
+    const fee = "500000";
+    const id = "123245";
+
+    const callers = await ethers.getSigners();
+    const mainAccount = callers[0];
+
+    mockRegistrar.ownerOf.whenCalledWith(id).returns(mainAccount.address);
+    await zAuction.connect(mainAccount).setTopLevelDomainFee(id, fee);
+    const tx = zAuction.connect(mainAccount).setTopLevelDomainFee(id, fee);
+
+    await expect(tx).to.be.revertedWith("zAuction: Amount is already set");
+  });
+  it("Gets the top level parent of a domain that is already the top", async () => {
+    // Case where id given is already the top level domain id
+    mockRegistrar.parentOf.returns(0);
+    const id = "12345";
+    const topLevelId = await zAuction.topLevelDomainIdOf(id);
+    expect(topLevelId).to.equal(id);
+  });
+  it("Gets the top level parent when the id given is not already the top", async () => {
+    // Case where id given is not the top level domain id
+    mockRegistrar.parentOf.whenCalledWith("3").returns("2");
+    mockRegistrar.parentOf.whenCalledWith("2").returns("1");
+    mockRegistrar.parentOf.whenCalledWith("1").returns("0");
+    const id = "3";
+    const TopLevelId = await zAuction.topLevelDomainIdOf(id);
+    expect(TopLevelId).to.equal("1");
   });
 });
