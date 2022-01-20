2befeed62f203cb50089aa37feb25e886e14dd06
James Earle <j_earle@hotmail.com>
2022-01-20 15:54:32 -0800

    deploy zauction to rinkeby

--- scripts/deploy-zauction.ts
+++ scripts/deploy-zauction.ts
@@ -19,8 +19,14 @@ import {
 import { Contract } from "ethers";
 const logger = getLogger("scripts::deploy-zauction");
 
-const tokenAddress = "0x50A0A3E9873D7e7d306299a75Dc05bd3Ab2d251F"; //kovan addresses, change to correct later
-const registrarAddress = "0xC613fCc3f81cC2888C5Cccc1620212420FFe4931";
+// const tokenAddress = "0x50A0A3E9873D7e7d306299a75Dc05bd3Ab2d251F"; //kovan addresses, change to correct later
+// const registrarAddress = "0xC613fCc3f81cC2888C5Cccc1620212420FFe4931";
+
+// Rinkeby addresses
+const tokenAddress = "0x3Ae5d499cfb8FB645708CC6DA599C90e64b33A79";
+const registrarAddress = "0xa4F6C921f914ff7972D7C55c15f015419326e0Ca";
+// No legacy address for Rinkeby exists but must have valid address, using kovan zauction
+const legacyZAuctionAddress = "0x18A804a028aAf1F30082E91d2947734961Dd7f89";
 
 interface DeployedContract {
   isUpgradable: boolean;
@@ -53,7 +59,7 @@ async function main() {
 
   const instance = await upgrades.deployProxy(
     zauctionfactory,
-    [tokenAddress, registrarAddress],
+    [tokenAddress, registrarAddress, legacyZAuctionAddress],
     {
       initializer: "initialize",
     }
@@ -91,7 +97,7 @@ async function main() {
       tokenAddress,
       registrarAddress,
     },
-    "wilder-prod"
+    "rinkeby"
   );
 
   if (deploymentData.implementationAddress) {
